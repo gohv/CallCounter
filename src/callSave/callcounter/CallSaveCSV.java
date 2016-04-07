bfdcef8415da69018c874c92c4f9736bbfc65547
Georgi Hristov <georgi.hristov@bluesource.co.uk>
2016-04-07 16:27:26 +0300

    Save to .txt added with date

--- src/callSave/callcounter/CallSaveCSV.java
+++ src/callSave/callcounter/CallSaveCSV.java
@@ -1,5 +1,29 @@
 package callSave.callcounter;
+import java.util.Date;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
 
 public class CallSaveCSV {
+	Date date = new Date();
+	
+	String getDate = date.toString();
+	
+	public void save(String save) throws FileNotFoundException, IOException {
+
+		try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("CSV/calls.txt", true))))
+		{
+			
+				writer.write(getDate);
+				writer.write(save);
+				writer.write("\n");
+			
+		}
+
+	}
 
 }
